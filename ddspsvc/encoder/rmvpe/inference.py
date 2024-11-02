8ff577106e1339ce4cf737d9747175ab031c0500
HuanLinOTO <linsmc@126.com>
2024-11-03 00:56:12 +0800

    ✨ feat(ddsp rmvpe): 修map_location无cuda

--- ddspsvc/encoder/rmvpe/inference.py
+++ ddspsvc/encoder/rmvpe/inference.py
@@ -11,7 +11,10 @@ class RMVPE:
     def __init__(self, model_path, hop_length=160):
         self.resample_kernel = {}
         model = E2E0(4, 1, (2, 2))
-        ckpt = torch.load(model_path)
+        ckpt = torch.load(
+            model_path,
+            map_location="cuda" if torch.cuda.is_available() else "cpu",
+        )
         model.load_state_dict(ckpt["model"], strict=False)
         model.eval()
         self.model = model
